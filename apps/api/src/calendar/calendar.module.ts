9a17e26c70b221c5b3b9f4d91b1a8926dbcce2de
amin4554 <anfs4554@gmail.com>
2026-05-24 23:16:37 +0200

    fix

--- apps/api/src/calendar/calendar.module.ts
+++ apps/api/src/calendar/calendar.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CalendarController } from './calendar.controller';
 import { CalendarService } from './calendar.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [CalendarController],
   providers: [CalendarService],
   exports: [CalendarService],
